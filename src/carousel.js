4e054555fc235c361760c66cf8746a8e4030e4cb
mcsnipe97 <anujsoni1997@gmail.com>
2020-09-17 13:05:51 +0530

    Add Details route, add custom babel config, add Carousel
    Used custom babel preset for typescript public class propersites to be used in modern javascript.

---
+++ src/carousel.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  state = {
+    photos: [],
+    active: 0
+  };
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+
+  //arrow function to fix the `this` context
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index //+ for converting str to int
+    });
+  };
+
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {this.props.media.map((photo, index) => (
+            //eslint-disable-next-line
+            <img
+              key={photo.large}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo.large}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+            // Could have this run correctly, but just leaving it here for now.
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
--- src/details.js
+++ src/details.js
@@ -1,11 +1,42 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./carousel";
 
-const Details = () => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  state = { loading: true }; //Proposal for JS but not a real thing quite yet.
+
+  componentDidMount() {
+    //Arrow function is must for `this` usage in class components
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error());
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+    const { animal, breed, location, description, name, media } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
